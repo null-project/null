b5295fac37d59044fb0a6eda9f50ad898f2d1434
null-project <wwl1f@vmani.com>
2018-03-07 11:44:10 +0100

    Fix main.h

--- src/main.h
+++ src/main.h
@@ -37,8 +37,8 @@ static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 170000;
 static const unsigned int MAX_STANDARD_TX_SIZE = MAX_BLOCK_SIZE_GEN/2.5; // 100KB tx size limit
 /** The maximum allowed number of signature check operations in a block (network rule) */
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
-/** The maximum number of orphan transactions kept in memory */
-static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
+/** Default for -maxorphantx, maximum number of orphan transactions kept in memory */
+static const unsigned int DEFAULT_MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
 /** The maximum size of a blk?????.dat file (since 0.8) */
 static const unsigned int MAX_BLOCKFILE_SIZE = 134217728; // 128 MiB
 /** The pre-allocation chunk size for blk?????.dat files (since 0.8) */
